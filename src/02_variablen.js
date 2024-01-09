19d5e0ddc3d579a1a0f6ef6b8d1d9e6cb7098994
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-09 10:51:07 +0000

    02 / Deklaration + Wertzuweisung

---
+++ src/02_variablen.js
@@ -0,0 +1,23 @@
+
+// Zeilenkommentar
+
+/*
+ Block-Kommentar
+*/ 
+
+/***** Variablen 01 *******/
+
+// console.log("Hi!"); // Ausgabe
+// console.log(Hi); // Ausgabe
+
+/* Deklaration + Wertzuweisung */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(firstName + familyName);
+console.log(firstName + " " + familyName);
+console.log("Hi, ich bin " + firstName + " " + familyName +"!");
+
