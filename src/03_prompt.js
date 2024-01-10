9871e48e2010cc590fb90026f4f39ed124f5ac36
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-10 11:22:12 +0000

    04 / Code Varianten

--- src/03_prompt.js
+++ src/03_prompt.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')({sigint: true});
 
 // Alles muss man selber machen lassen!...
 let firstName = prompt("Vorname: ");
-//console.log(firstName);
+// console.log(firstName);
 let lastName = prompt("Nachname: ");
-//console.log(lastName);
+// console.log(lastName);
 console.log("Username: " + firstName + " " + lastName);
