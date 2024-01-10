ec7d0ffbc090bca9c1a61ae7b333748c91401950
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-10 10:07:41 +0000

    03 / Prompt

---
+++ src/03_prompt.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+// Alles muss man selber machen lassen!...
+let firstName = prompt("Vorname: ");
+//console.log(firstName);
+let lastName = prompt("Nachname: ");
+//console.log(lastName);
+console.log("Username: " + firstName + " " + lastName);
---
+++ src/04_codechallenge_I.js
@@ -0,0 +1,16 @@
+
+/*********   CODE CHALLENGE 01 **********/
+ 
+/*
+Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+Berechnen Sie die Summe der beiden Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+"Die Summe der Zahlen ist: summe"
+*/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let firstNumber = prompt("Erste Zahl: ")
+let secondNumber = prompt("Zweite Zahl: ")
+console.log("Summe: " + firstNumber + secondNumber)
