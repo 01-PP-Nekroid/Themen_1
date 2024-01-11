0c4b8b2074b9dc32789c60dc8777406d40644afb
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-11 08:24:12 +0000

    05 / Test`s

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,7 +3,7 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 2000;
+const birthYearJohn = /*2000*/ 1980;
 const birthYearMark = 1990;
 
 // Berechnung Alter
@@ -13,6 +13,15 @@ ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
+console.log("age John: " + ageJohn);
+console.log("age Mark: " + ageMark);
 console.log("Datum: " + date);
+
+
+// Deklaration
+let isJohnOlder; // Is John older? --> y/n
+isJohnOlder = (ageJohn > ageMark); // Test größer/kleiner
+isJohnEqual = (ageJohn == ageMark); // Test Gleichheit
+console.log("is John Older? " + isJohnOlder);
+console.log("is John Equal? " + isJohnEqual);
+
