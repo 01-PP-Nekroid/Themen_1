c6f534e17dd5c1ccd6e9f5d553dc18740836d072
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-12 10:00:42 +0000

    08 / Funktionen 2a/b

--- src/08_funtionen_I.js
+++ src/08_funtionen_I.js
@@ -11,3 +11,32 @@ function test()
 }
 
 
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Call
+ausgabeNamen();
+
+// Funktion
+function ausgabeNamen()
+{
+    // interne Variable | "What happens in VEGAS..."
+    let firstName = "Kevin";
+    console.log("Hallo, Bores");
+    console.log("Hallo, " + firstName);
+}
+
+// console.log(firstName); // Fehler : scope!
+
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// Argumente sind Daten für Parameter
+ausgabeNamen2("Marie"); // Argument
+
+function ausgabeNamen2(firstName) // Parameter
+{
+    console.log("Hallo, " + firstName);
+}
+
