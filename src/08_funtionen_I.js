049eddacd8467606a6f2205ae088807de0097b61
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-12 09:14:35 +0000

    08 / 1. Kapselung von Codeblöcken

---
+++ src/08_funtionen_I.js
@@ -0,0 +1,13 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaurruf | call
+// Test();
+
+// Funktionsrumpf / Funktionsdeklaration
+function test()
+{
+    console.log("Hallo");
+}
+
+
