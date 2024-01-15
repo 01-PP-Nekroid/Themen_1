594aec3ddd1fb1c8df235fab7a0afd5226d27c29
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-15 09:33:24 +0000

    09 / mul() ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -30,6 +30,18 @@ function subtract(a,b)
 	return a - b;
 }
 
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+
+function multiply(a,b)
+{
+	return a * b;
+}
+
+
+
 // module: output | test:
 // output("hello");
 // output(2);
