ee5e0c1d3ccc41acd8cb14fc51442ff1dc08a42a
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-15 11:18:42 +0000

    09 / calculator

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,10 +10,46 @@
 4. Ausgabe in Konsole : check
 */
 
+
+const ERROR_STR_DIV = /* "Division durch 0 ist nicht definiert." */ "I`m sorry, Dave... I`m afraid. I can`t do that!";
+const ERROR_STR_GEN = /* "Irgendetwas ist schief gegangen! */ "I`m so sorry, Dave..."
+
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+function calculator(a,b,op)
+{
+	switch (op)
+	{
+		case "+": // add()
+			return add(a,b);
+	
+		case "-": // sub()
+			return subtract(a,b);
+		
+		case "*": // mul()
+			return multiply(a,b);
+
+		case ":": // div()
+		case "/": // div()
+			return divide(a,b);
+
+		default: // Error
+			return ERROR_STR_GEN;
+	}
+}
+
+
 // module: addition a+b | test
 //output(add(2,2));
 //output(add(4,-5));
-
 function add(a,b)
 {
 	return a + b;
@@ -25,7 +61,6 @@ function add(a,b)
 // output(subtract(3,0));
 // output(subtract(3,-4));
 // output(subtract(0,4));
-
 function subtract(a,b)
 {
 	return a - b;
@@ -36,24 +71,19 @@ function subtract(a,b)
 // output(multiply(3,2));
 // output(multiply(3,-2));
 // output(multiply(3,0));
-
 function multiply(a,b)
 {
 	return a * b;
 }
 
 
-const ERROR_STR_DIV = /* "Division durch 0 ist nicht definiert." */ "Sie denken, dass Sie diese Aufgabe berechnet haben wollen. Wollen Sie aber nicht!";
-
-
 // module: division a / b |  test
 
-output(divide(4,2));
-output(divide(3,2));
-output(divide(3,-2));
-output(divide(0,2));
-output(divide(3,0));
-
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
 function divide(a,b)
 {
 	if (b == 0)
