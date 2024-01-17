a324505ebe0adb3225c4a590287e061cd29fafe8
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-17 09:46:00 +0000

    11 / 02a, 01b

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,10 +5,10 @@
 
 
 /*** 02. Theorie: Array */
-let arr;
+// let arr;
 // arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,11,7,8,10];
+// arr = []; // Literal
+// arr = [2,11,7,8,10];
 
 // output(arr);
 // output(arr.length); // length = Anzahl der Elemente
@@ -16,30 +16,39 @@ arr = [2,11,7,8,10];
 // output(arr[arr.length-1]); // letzte pos. / allgemein
 
 
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+// Inkrement (untergrenze --> ober Grenze)
+/*for (let i = 10; i < 50; i++)
+{
+    output(i);
+}*/
+
+
+// Dekrement (obere Grenze --> untere Grenze)
+/*for (let i = 10; i > 0; i--)
+{
+    output(i);
+}*/
+
+
+// Inkrement (var. Schrittweite)
+/*for (let i = 10; i <= 100; i += 5)
+{
+    output(i);
+}*/
+
+
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+/*let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+for (let i = 0; i < arr.length; i++)
+{
+    // output(i);
+    output(arr[i]);
+}*/
+
 /*Praxis*/
-
-/*** 01a. Funktionalität mit Array 1 */
-// Kritik | Überlegung
-// word1,word2, .... wordN :: Semantische Struktur
-// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
-// --> Transformation semantisches Problem --> numerisches Problem
-output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentenceArr(arr) {
-const GAP = " ";
-    const PUNCT = ".";
-    let str =   arr[0] + GAP +
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] +
-                PUNCT;
-
-
-
-return str;
-}
-
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
@@ -60,6 +69,47 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 }
 
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+
+
+return str;
+}
+
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) 
+{
+    const GAP = " ";
+    const PUNCT = ".";
+    let str;
+    
+    for (let i = 0; i < arr.length; i++)
+    {
+         output(arr[i]);
+    }
+
+    return str;
+}
+
 
 
 // Modul: Ausgabe in Konsole : Test
@@ -67,5 +117,3 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
