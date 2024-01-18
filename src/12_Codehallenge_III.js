7fdd86ab778a5f1d5b70e10c0bdaaca35a890f6e
Nekroid <155630807+Nekroid@users.noreply.github.com>
2024-01-18 07:52:32 +0000

    12 / ...

--- src/12_Codehallenge_III.js
+++ src/12_Codehallenge_III.js
@@ -17,37 +17,17 @@ output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
-function getSentence(arr,op) {
-    const GAP = " ";
-    const PUNCT = getPunct(op);  // . ? !
-    let str = ""; 
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i != arr.length-1) {
-            str += arr[i] + GAP;
-        } else {
-            str += arr[i] + PUNCT; 
-        }
-    }
-     return str;
+function getSentence(arr,op)
+{
+   // ???
 }
 
-function getPunct(op) {
-    switch (op) {
-        case "S":
-           return "."; 
-        case "Q":
-            return "?";
-        case "E":
-            return "!";
-        default:
-            return "#";
-    }
-}
 
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
-function output(outputData) {
+function output(outputData)
+{
     console.log(outputData);
 }
